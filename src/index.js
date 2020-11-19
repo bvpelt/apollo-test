6740d1dc146b4ceb4f00ddb813c57d840240135f
brtvnplt <bart.vanpelt@gmail.com>
2020-11-19 08:40:02 +0100

    Added plugin

--- src/index.js
+++ src/index.js
@@ -4,56 +4,12 @@ const { ApolloServerPluginUsageReporting } = require("apollo-server-core");
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const OzonAPI = require('./datasources/ozon');
+const myPlugin = require('./plugin')
 
 // set up any dataSources our resolvers need
 const dataSources = () => ({
   ozonAPI: new OzonAPI()
 });
-
-
-const myPlugin = {
-  requestDidStart(requestContext) {
-    return {
-      parsingDidStart() {
-        return (err) => {
-          if (err) {
-            console.error(err);
-          }
-        }
-      },
-      validationDidStart() {
-        // This end hook is unique in that it can receive an array of errors,
-        // which will contain every validation error that occurred.
-        return (errs) => {
-          if (errs) {
-            errs.forEach(err => console.error(err));
-          }
-        }
-      },
-      executionDidStart() {
-        return (err) => {
-          if (err) {
-            console.error(err);
-          }
-        }
-      },
-
-      /* The `parsingDidStart` request lifecycle event fires
-         when parsing begins. The event is scoped within an
-         associated `requestDidStart` server lifecycle event. */
-      parsingDidStart(requestContext) {
-        console.log('Parsing started!')
-      },
-      responseForOperation(requestContext) {        
-        console.log('request operationName: ', requestContext.operationName);
-        console.log('request source: ', requestContext.source);
-        console.log('request document: ', requestContext.document);
-        console.log('request logger: ', requestContext.logger);
-      },
-
-    }
-  }
-}
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
